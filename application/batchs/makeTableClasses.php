507c721152dad2fee0d7db295cede1bf265f24f1
Jlake Ou <ouzhiwei@gmail.com>
2011-07-05 18:53:02 +0900

    add batch to make table class automatically

---
+++ application/batchs/makeTableClasses.php
@@ -0,0 +1,117 @@
+<?php
+require_once 'CliBootstrap.php';
+
+class Batch_MakeTableClasses
+{
+    protected $_opts;
+    protected $_db;
+    protected $_dbw;
+    protected $_logger;
+    protected $_rootPath;
+
+    public function __construct($rootPath)
+    {
+        $this->_rootPath = $rootPath;
+    }
+
+    protected function _init()
+    {
+        try {
+            $this->_opts = new Zend_Console_Getopt(
+                array(
+                    'help|h' => 'Displays usage information.',
+                    'force|f' => 'Force overwrite.',
+                )
+            );
+            $this->_opts->parse();
+        } catch (Zend_Console_Getopt_Exception $e) {
+            exit($e->getMessage() ."\n\n". $e->getUsageMessage());
+        }
+
+        if(isset($this->_opts->help)) {
+            echo $this->_opts->getUsageMessage();
+            exit;
+        }
+
+        $this->_db = Zend_Registry::get('db');
+        $this->_dbw = Zend_Registry::get('dbw');
+        $this->_logger = Zend_Registry::get('batch_logger');
+    }
+
+    /**
+     * テーブル名からクラス名の取得
+     */
+    public static function _getClassName($tableName)
+    {
+        $words = explode('_', $tableName);
+        foreach($words as &$word) {
+            $word = ucfirst($word);
+        }
+        return implode('', $words);
+    }
+
+    /** 
+    * DbTable クラスファイルの作成
+    */
+    private function _makeClassFile($tableName) {
+        $className = $this->_getClassName($tableName);
+        $filePath = $this->_rootPath.'/'.$className.'.php';
+        if(!isset($this->_opts->force) && file_exists($filePath)) {
+            return;
+        }
+
+        echo "$tableName --> $className \n";
+
+        $template = "<?php
+class Lib_Db_Table_%className% extends Lib_Db_Table
+{
+    protected \$_name = '%tableName%';
+    protected \$_primary = array(%primaryKeys%);
+}
+";
+
+        $pkeys = $this->_db->fetchCol("SELECT `COLUMN_NAME`
+            FROM `information_schema`.`COLUMNS`
+            WHERE (`TABLE_NAME` = 'campaign_tbl')
+                AND (`COLUMN_KEY` = 'PRI')
+        ");
+
+        $contents = Lib_Util_Message::replace($template, array(
+                'className' => $className,
+                'tableName' => $tableName,
+                'primaryKeys' => "'".implode("', '", $pkeys)."'"
+            )
+        );
+        $contents = mb_convert_encoding($contents, 'UTF-8', 'auto');
+        try {
+            if(file_exists($filePath) && !is_writable($filePath)) {
+                echo "Could not overwrite $filePath \n";
+                return;
+            }
+            file_put_contents($filePath, $contents);
+        } catch(Exception $e) {
+            echo "Failed to write file $filePath \n";
+        }
+    }
+    
+    public function run()
+    {
+        $this->_init();
+        $this->_logger->log('START run ' . __FILE__, Zend_Log::INFO);
+
+        if(is_writable($this->_rootPath)) {
+            $tableNames = $this->_db->fetchCol("SHOW TABLES");
+            foreach($tableNames as $tableName) {
+                $this->_makeClassFile($tableName);
+            }
+        } else {
+            echo $this->_rootPath . " is not writable.\n";
+        }
+
+        $this->_logger->log('END run ' . __FILE__, Zend_Log::INFO);
+    }
+}
+
+$batch = new Batch_MakeTableClasses( APPLICATION_PATH.'/libs/Db/Table' );
+$batch->run();
+
