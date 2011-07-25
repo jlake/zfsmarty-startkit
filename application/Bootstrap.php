4ab4229509aefadda5d9aa774a26fadd6bd8f857
Jlake Ou <ouzhiwei@gmail.com>
2011-07-25 19:00:32 +0900

    add firebug logger

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -115,6 +115,10 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             $logger = new Zend_Log(new Zend_Log_Writer_Stream("$logDir/$logFile"));
             Zend_Registry::set($folder.'_logger', $logger);
         }
+        if(APPLICATION_ENV == 'development') {
+            $firebug = new Zend_Log(new Zend_Log_Writer_Firebug());
+            Zend_Registry::set('firebug', $firebug);
+        }
     }
 
     protected function _initCache()
