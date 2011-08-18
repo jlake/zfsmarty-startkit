ffda0d157dd9fccad5b6d5e56e020d464692b997
Jlake Ou <ouzhiwei@gmail.com>
2011-08-18 18:28:05 +0900

    update jqGrid demo

--- application/libs/Db/Util.php
+++ application/libs/Db/Util.php
@@ -36,4 +36,95 @@ class Lib_Db_Util
         }
         return 'Lib_Db_Table_' . join('', $words);
     }
+
+
+    /**
+     * 検索条件でWhere条件文字列の取得(jqGrid用)
+     * 
+     * @param $field  フィルド名
+     * @param $op  比較タイプ
+     * @param $value  値
+     * @return  string   Where条件文字列
+     */
+    public static function getWhereString($field, $op, $value)
+    {
+        $db = Zend_Registry::get('db');
+        /*
+        bw - begins with ( LIKE val% )
+        eq - equal ( = )
+        ne - not equal ( <> )
+        lt - little ( < )
+        le - little or equal ( <= )
+        gt - greater ( > )
+        ge - greater or equal ( >= )
+        ew - ends with (LIKE %val )
+        cn - contain (LIKE %val% )
+        */
+        $whereStr = '';
+        switch($op) {
+            case 'eq':
+                $whereStr = $field ." = " . $db->quote($value);
+                break;
+            case 'ne':
+                $whereStr = $field ." <> " . $db->quote($value);
+                break;
+            case 'lt':
+                $whereStr = $field ." < " . $db->quote($value);
+                break;
+            case 'le':
+                $whereStr = $field ." <= " . $db->quote($value);
+                break;
+            case 'gt':
+                $whereStr = $field ." > " . $db->quote($value);
+                break;
+            case 'ge':
+                $whereStr = $field ." >= " . $db->quote($value);
+                break;
+            case 'bw':
+                $whereStr = $field ." LIKE " . $db->quote($value . '%');
+                break;
+            case 'ew':
+                $whereStr = $field ." LIKE " . $db->quote('%' . $value);
+                break;
+            case 'cn':
+                $whereStr = $field ." LIKE " . $db->quote('%' . $value. '%');
+                break;
+            default:
+                $whereStr = $field ." = " . $db->quote($value);
+                break;
+        }
+        return $whereStr;
+    }
+    
+    /**
+     * フィルタでWhere条件配列の取得(jqGrid用)
+     * 
+     * @param $filter  フィルタ
+     * @param $aliasMap  テーブル別名 → カラム のマップ
+     * @return  Array   Where条件配列
+     */
+    public static function getWhereByFilter($filter, $aliasMap = array())
+    {
+        if(empty($filter)) {
+            //フィルタ なし
+            return $whereArray;
+        }
+        if(!is_array($filter)) {
+            $filter = json_decode($filter, true);
+        }
+        $whereArray = array();
+        //フィルタ あり
+        //	filters: {"groupOp":"AND","rules":[{"field":"id","op":"lt","data":"1"},{"field":"inf1","op":"cn","data":"7"}]}
+        foreach($filter['rules'] as $rule) {
+            $field = $rule['field'];
+            foreach($aliasMap as $alias => $fields) {
+                if(in_array($field, $fields)) {
+                    $field = $alias.'.'.$field;
+                    break;
+                }
+            }
+            $whereArray[] = self::getWhereString($field, $rule['op'], $rule['data']);
+        }
+        return $whereArray;
+    }
 }
--- application/modules/admin/models/GridDemo.php
+++ application/modules/admin/models/GridDemo.php
@@ -47,7 +47,7 @@ class Admin_Model_GridDemo
             }
             $dataSelect->order($sortOrder);
         }
-        Lib_Util_Log::firebug($dataSelect->__toString());
+        //Lib_Util_Log::firebug($dataSelect->__toString());
         $rows = array();
         foreach($this->_db->fetchAll($dataSelect) as $row) {
             $rows[] = array(
@@ -92,25 +92,25 @@ class Admin_Model_GridDemo
             );
 
         if(!empty($params['_search'])) {
-            if(!empty($params['searchField'])) {
-                $where =  self::getWhereString($params['searchField'], $params['searchOper'], $params['searchString']);
-                $dataSelect->where($where);
-                $countSelect->where($where);
-            }
             //フィルタ
-            if(!empty($params['filter'])) {
-                $whereArray = self::getWhereByFilter($params['filter'], $aliasMap);
-                if($params['filter']['groupOp'] == 'AND') {
-                    foreach($whereArray as $where) {
-                        $dataSelect->where($where);
-                        $countSelect->where($where);
-                    }
-                } else {
+            if(!empty($params['filters'])) {
+                $filters = json_decode($params['filters'], true);
+                $whereArray = Lib_Db_Util::getWhereByFilter($filters, $aliasMap);
+                if($filters['groupOp'] == 'OR') {
                     foreach($whereArray as $where) {
                         $dataSelect->orWhere($where);
                         $countSelect->orWhere($where);
                     }
+                } else {
+                    foreach($whereArray as $where) {
+                        $dataSelect->where($where);
+                        $countSelect->where($where);
+                    }
                 }
+            } else if(!empty($params['searchField'])) {
+                $where =  Lib_Db_Util::getWhereString($params['searchField'], $params['searchOper'], $params['searchString']);
+                $dataSelect->where($where);
+                $countSelect->where($where);
             }
         }
 
@@ -142,93 +142,5 @@ class Admin_Model_GridDemo
             'records' => $count,
             'rows' => $rows
         );
-    }
-
-    /**
-     * 検索条件でWhere条件文字列の取得
-     * 
-     * @param $field  フィルド名
-     * @param $op  比較タイプ
-     * @param $value  値
-     * @return  string   Where条件文字列
-     */
-    public static function getWhereString($field, $op, $value)
-    {
-        Lib_Util_Log::firebug($field.$op.$value);
-        $db = Zend_Registry::get('db');
-        /*
-        bw - begins with ( LIKE val% )
-        eq - equal ( = )
-        ne - not equal ( <> )
-        lt - little ( < )
-        le - little or equal ( <= )
-        gt - greater ( > )
-        ge - greater or equal ( >= )
-        ew - ends with (LIKE %val )
-        cn - contain (LIKE %val% )
-        */
-        $whereStr = '';
-        switch($op) {
-            case 'eq':
-                $whereStr = $field ." = " . $db->quote($value);
-                break;
-            case 'ne':
-                $whereStr = $field ." <> " . $db->quote($value);
-                break;
-            case 'lt':
-                $whereStr = $field ." < " . $db->quote($value);
-                break;
-            case 'le':
-                $whereStr = $field ." <= " . $db->quote($value);
-                break;
-            case 'gt':
-                $whereStr = $field ." > " . $db->quote($value);
-                break;
-            case 'ge':
-                $whereStr = $field ." >= " . $db->quote($value);
-                break;
-            case 'bw':
-                $whereStr = $field ." LIKE " . $db->quote($value . '%');
-                break;
-            case 'ew':
-                $whereStr = $field ." LIKE " . $db->quote('%' . $value);
-                break;
-            case 'cn':
-                $whereStr = $field ." LIKE " . $db->quote('%' . $value. '%');
-                break;
-            default:
-                $whereStr = $field ." = " . $db->quote($value);
-                break;
-        }
-        return $whereStr;
-    }
-    
-    /**
-     * フィルタでWhere条件配列の取得
-     * 
-     * @param $filter  フィルタ
-     * @param $aliasMap  テーブル別名 → カラム のマップ
-     * @return  Array   Where条件配列
-     */
-    public static function getWhereByFilter($filter, $aliasMap = array())
-    {
-        if(empty($filter)) {
-            //フィルタ なし
-            return $whereArray;
-        }
-        $whereArray = array();
-        //フィルタ あり
-        //	filter: {"groupOp":"AND","rules":[{"field":"a.total","op":"lt","data":"1000"},{"field":"a.tax","op":"lt","data":"100"}],"groups":[]}
-        foreach($filter['rules'] as $rule) {
-            $field = preg_replace('/^\w\./', '', $rule[' field']);
-            foreach($aliasMap as $alias => $fields) {
-                if(in_array($field, $fields)) {
-                    $field = $alias.'.'.$field;
-                    break;
-                }
-            }
-            $whereArray[] = self::getWhereString($field, $rule['op'], $rule['data']);
-        }
-        return $whereArray;
     }
 }
