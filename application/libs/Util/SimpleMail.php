b6d0b2de6f8fafe0df4e6a9e3da926836a4424d7
Jlake Ou <ouzhiwei@gmail.com>
2011-09-22 12:32:59 +0900

    update SimpleMail class for send attachments

--- application/libs/Util/SimpleMail.php
+++ application/libs/Util/SimpleMail.php
@@ -7,7 +7,7 @@ class Lib_Util_SimpleMail
 {
     /**
      * メール送信機能
-     * @param $params パラメータの配列(キー： subject, body, from, to, replace)
+     * @param $params パラメータの配列(キー： subject, body, from, to, replace, attachments)
      * @return void
      */
     public static function send($params)
@@ -28,10 +28,22 @@ class Lib_Util_SimpleMail
         foreach($params['to'] as $to) {
             $mailObj->addTo(trim($to));
         }
+        if(isset($params['attachments'])) {
+            // 添付ファイル付与
+            foreach($params['attachments'] as $attachFile) {
+                // ファイル確認出来る場合のみ
+                if(file_exists($attachFile)) {
+                    // ファイル名をUTF-8に統一
+                    $attachName = mb_convert_encoding(basename($attachFile), "UTF-8", 'auto');
+                    $mailObj->createAttachment(Lib_Util_File::read($attachFile), Zend_Mime::TYPE_OCTETSTREAM, Zend_Mime::DISPOSITION_ATTACHMENT, Zend_Mime::ENCODING_BASE64, $attachName);
+                }
+            }
+        }
+
         //エラーメールの返信先
         $tr = new Zend_Mail_Transport_Sendmail('-f'.trim($params['from']));
         $mailObj->setDefaultTransport($tr);
-        
+
         $mailObj->send();
     }
 }
