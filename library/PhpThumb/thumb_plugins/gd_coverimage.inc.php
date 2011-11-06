24cea12d511d8ec5ee0a617215ca2877163b3609
Jlake Ou <ouzhiwei@gmail.com>
2011-11-06 15:10:25 +0900

    update GDcoverImage plugin

--- library/PhpThumb/thumb_plugins/gd_coverimage.inc.php
+++ library/PhpThumb/thumb_plugins/gd_coverimage.inc.php
@@ -1,34 +1,12 @@
 <?php
 /**
- * GD Reflection Lib Plugin Definition File
+ * 書影画像作成 Plugin
  * 
- * This file contains the plugin definition for the GD Reflection Lib for PHP Thumb
- * 
- * PHP Version 5 with GD 2.0+
- * PhpThumb : PHP Thumb Library <http://phpthumb.gxdlabs.com>
- * Copyright (c) 2009, Ian Selby/Gen X Design
- * 
- * Author(s): Ian Selby <ian@gen-x-design.com>
- * 
- * Licensed under the MIT License
- * Redistributions of files must retain the above copyright notice.
- * 
- * @author Ian Selby <ian@gen-x-design.com>
- * @copyright Copyright (c) 2009 Gen X Design
- * @link http://phpthumb.gxdlabs.com
- * @license http://www.opensource.org/licenses/mit-license.php The MIT License
- * @version 3.0
- * @package PhpThumb
- * @filesource
- */
-
-/**
- * GD Reflection Lib Plugin
- * 
- * This plugin allows you to create those fun Apple(tm)-style reflections in your images
+ * 画像をリサイズして間中に表示、背景は白色にします
  * 
  * @package PhpThumb
  * @subpackage Plugins
+ * @author ou
  */
 class GdCoverImageLib
 {
@@ -61,7 +39,7 @@ class GdCoverImageLib
 
         $newX = 0;
         $newY = 0;
-        if($coverWidth < $coverHeight) {
+        if($coverWidth <= $coverHeight) {
             if($width >= $height) {
                 $newWidth = $coverWidth;
                 $newHeight = ceil($newWidth * $height / $width);
@@ -82,12 +60,11 @@ class GdCoverImageLib
                 $newY = ceil(($coverHeight - $newHeight) / 2);
             }
         }
-
-/*
-Lib_Util_Log::log('site', "width = $width; height = $height");
-Lib_Util_Log::log('site', "newWidth = $newWidth; newHeight = $newHeight");
-Lib_Util_Log::log('site', "newX = $newX; newY = $newY");
-*/
+        /*
+        Lib_Util_Log::log('site', "width = $width; height = $height");
+        Lib_Util_Log::log('site', "newWidth = $newWidth; newHeight = $newHeight");
+        Lib_Util_Log::log('site', "newX = $newX; newY = $newY");
+        */
         imagecopyresized($this->workingImage, $this->oldImage, $newX, $newY, 0, 0, $newWidth, $newHeight, $width, $height);
 
         $this->parentInstance->setOldImage($this->workingImage);
