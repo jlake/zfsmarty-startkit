7c970c156c84a4229e635fbf98d543a609fa3e1a
Jlake Ou <ouzhiwei@gmail.com>
2011-11-10 12:36:15 +0900

    update PhpThumb's plugin

--- library/PhpThumb/thumb_plugins/gd_coverimage.inc.php
+++ library/PhpThumb/thumb_plugins/gd_coverimage.inc.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * 書影画像作成 Plugin
- * 
+ *
  * 画像をリサイズして間中に表示、背景は白色にします
- * 
+ *
  * @package PhpThumb
  * @subpackage Plugins
  * @author ou
@@ -12,7 +12,7 @@ class GdCoverImageLib
 {
     /**
      * Instance of GdThumb passed to this class
-     * 
+     *
      * @var GdThumb
      */
     protected $parentInstance;
@@ -20,7 +20,7 @@ class GdCoverImageLib
     protected $workingImage;
     protected $oldImage;
     protected $options;
-    
+
     public function createCoverImage($coverWidth, $coverHeight, &$that)
     {
         // bring stuff from the parent class into this class...
@@ -29,7 +29,7 @@ class GdCoverImageLib
         $this->workingImage         = $this->parentInstance->getWorkingImage();
         $this->oldImage             = $this->parentInstance->getOldImage();
         $this->options              = $this->parentInstance->getOptions();
-        
+
         $width                      = $this->currentDimensions['width'];
         $height                     = $this->currentDimensions['height'];
 
@@ -39,33 +39,28 @@ class GdCoverImageLib
 
         $newX = 0;
         $newY = 0;
-        if($coverWidth <= $coverHeight) {
-            if($width >= $height) {
-                $newWidth = $coverWidth;
-                $newHeight = ceil($newWidth * $height / $width);
-                $newY = ceil(($coverHeight - $newHeight) / 2);
-            } else {
-                $newHeight = $coverHeight;
-                $newWidth = ceil($newHeight * $width / $height);
-                $newX = ceil(($coverWidth - $newWidth) / 2);
-            }
-        } else {
-            if($height >= $width) {
-                $newHeight = $coverHeight;
-                $newWidth = ceil($newHeight * $width / $height);
-                $newX = ceil(($coverWidth - $newWidth) / 2);
-            } else {
-                $newWidth = $coverWidth;
-                $newHeight = ceil($newWidth * $height / $width);
-                $newY = ceil(($coverHeight - $newHeight) / 2);
-            }
+        $newWidth = $coverWidth;
+        $newHeight = $coverHeight;
+
+        $imgRatio = $width / $height;
+        $coverRatio = $coverWidth / $coverHeight; // 例: 96/150
+
+        if($imgRatio < $coverRatio) {
+            $newHeight = $coverHeight;
+            $newWidth = ceil($newHeight * $imgRatio);
+            $newX = ceil(($coverWidth - $newWidth) / 2);
+        } elseif ($imgRatio > $coverRatio) {
+            $newWidth = $coverWidth;
+            $newHeight = ceil($newWidth / $imgRatio);
+            $newY = ceil(($coverHeight - $newHeight) / 2);
         }
         /*
         Lib_Util_Log::log('site', "width = $width; height = $height");
         Lib_Util_Log::log('site', "newWidth = $newWidth; newHeight = $newHeight");
         Lib_Util_Log::log('site', "newX = $newX; newY = $newY");
         */
-        imagecopyresized($this->workingImage, $this->oldImage, $newX, $newY, 0, 0, $newWidth, $newHeight, $width, $height);
+        //imagecopyresized($this->workingImage, $this->oldImage, $newX, $newY, 0, 0, $newWidth, $newHeight, $width, $height);
+        imagecopyresampled($this->workingImage, $this->oldImage, $newX, $newY, 0, 0, $newWidth, $newHeight, $width, $height);
 
         $this->parentInstance->setOldImage($this->workingImage);
         $this->currentDimensions['width'] = $coverWidth;
