3c3808fd88848c3cc91fad3bea9d7f7f74cdf59e
Jlake Ou <ouzhiwei@gmail.com>
2011-11-15 10:00:41 +0900

    update Smarty3 to 3.1.5

--- scripts/sql/dummy.my.sql
+++ scripts/sql/dummy.my.sql
@@ -8,7 +8,7 @@ CREATE TABLE dummy (
 	inf2				VARCHAR(64),
 	set_date			TIMESTAMP,
 	set_nm				VARCHAR(64) DEFAULT 'system',
-	create_date			TIMESTAMP,
+	create_date			DATETIME,
 	PRIMARY KEY(id)
 );
 
@@ -18,31 +18,31 @@ CREATE TABLE dummy (
 */
 DELETE FROM dummy;
 INSERT INTO dummy(
-		id, inf1, inf2, set_nm, set_date
+		id, inf1, inf2, set_nm, create_date
 	) VALUES (
 		NULL, '1行1番', '1行2番', 'ou', CURRENT_TIMESTAMP
 	);    
 
 INSERT INTO dummy(
-		id, inf1, inf2, set_nm, set_date
+		id, inf1, inf2, set_nm, create_date
 	) VALUES (
 		NULL, '2行1番', '2行2番', 'ou', CURRENT_TIMESTAMP
 	);    
 
 INSERT INTO dummy(
-		id, inf1, inf2, set_nm, set_date
+		id, inf1, inf2, set_nm, create_date
 	) VALUES (
 		NULL, '3行1番', '3行2番', 'ou', CURRENT_TIMESTAMP
 	);    
 
 INSERT INTO dummy(
-		id, inf1, inf2, set_nm, set_date
+		id, inf1, inf2, set_nm, create_date
 	) VALUES (
 		NULL, '4行1番', '4行2番', 'ou', CURRENT_TIMESTAMP
 	);    
 
 INSERT INTO dummy(
-		id, inf1, inf2, set_nm, set_date
+		id, inf1, inf2, set_nm, create_date
 	) VALUES (
 		NULL, '5行1番', '5行2番', 'ou', CURRENT_TIMESTAMP
 	);    
